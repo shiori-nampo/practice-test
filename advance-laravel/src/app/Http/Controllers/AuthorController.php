c3ea71c45063a1544437b06331ab7ec8df249975
shiori0301 <st.ryo.9805@icloud.com>
2025-11-10 14:45:10 +0900

    Merge author-add changes into develop

--- advance-laravel/src/app/Http/Controllers/AuthorController.php
+++ advance-laravel/src/app/Http/Controllers/AuthorController.php
@@ -7,9 +7,25 @@ use Illuminate\Http\Request;
 
 class AuthorController extends Controller
 {
-    public function index()
+
+    public function add()
+    {
+        return view('add');
+    }
+
+    public function create(Request $request)
+    {
+        $form = $request->all();
+        Author::create($form);
+        return redirect('/');
+    }
+
+     public function index()
     {
         $authors = Author::all();
         return view('index',['authors' => $authors]);
     }
+
 }
+
+
--- advance-laravel/src/routes/web.php
+++ advance-laravel/src/routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/',[AuthorController::class,'index']);
 
+Route::get('/add',[AuthorController::class,'add']);
+Route::post('/add',[AuthorController::class,'create']);
+Route::get('/',[AuthorController::class,'index']);
