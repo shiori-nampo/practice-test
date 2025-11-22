182dbe63b3bc271a5dc043a6e5bb4ae6d91b0eb5
shiori0301 <st.ryo.9805@icloud.com>
2025-11-22 10:14:58 +0900

    Add fortify-setup/feature

---
+++ third-laravel/src/app/Providers/FortifyServiceProvider.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Laravel\Fortify\Fortify;
+use App\Actions\Fortify\CreateNewUser;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Http\Request;
+
+class FortifyServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+    Fortify::createUsersUsing(CreateNewUser::class);
+
+        Fortify::registerView(function () {
+            return view('auth.register');
+        });
+
+        Fortify::loginView(function () {
+            return view('auth.login');
+        });
+
+        RateLimiter::for('login',function (Request $request) {
+            $email = (string) $request->email;
+
+            return Limit::perMinute(10)->by($email . $request->ip());
+        });
+    }
+}
--- third-laravel/src/routes/web.php
+++ third-laravel/src/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware('auth')->group(function () {
+    Route::get('/',[AuthController::class,'index']);
+});
+Route::get('/login',function() {
+    return view('auth.login');
+});
+Route::get('/register',function() {
+    return view('auth.register');
 });
